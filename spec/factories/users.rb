a2a345e2c43d4f2788e97475f7d4e8fb2c5eebc4
Daniel Evani <danielevani97@gmail.com>
2022-01-29 21:53:17 -0300

    Testes de request configurados

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name { Faker::Name.name }
     email { Faker::Internet.email }
     password { "123456" }
-    password_confimation { "123456" }
+    password_confirmation { "123456" }
     profile { :admin }
   end
 end
